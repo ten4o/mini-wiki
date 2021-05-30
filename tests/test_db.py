4dc376e2a500dcf5bcf8b0261c851ed9fcffa378
Tencho Dimitrov <tencho@tencho.tk>
2021-05-30 12:43:13 +0300

    run tests in docker-compose

--- tests/test_db.py
+++ tests/test_db.py
@@ -98,7 +98,7 @@ class TestDb(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         print('==================== setUpClass')
-        cls.db = db.DB()
+        cls.db = db.DB(is_test=True)
 
     @classmethod
     def tearDownClass(cls):
