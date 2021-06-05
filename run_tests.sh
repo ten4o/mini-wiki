cd300c4ceb5624f0e9d826aca1053673eb5d0b24
Tencho Dimitrov <tencho@tencho.tk>
2021-06-05 08:14:44 +0300

    fix run_test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,5 @@
-docker-compose -f docker-compose-test.yml up
+docker-compose -f docker-compose-test.yml up \
+	 --abort-on-container-exit \
+    --exit-code-from web
+
+docker-compose -f docker-compose-test.yml down
